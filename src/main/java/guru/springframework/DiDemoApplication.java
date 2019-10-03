68909377a2cdd3704bc62a7672fea3c864403411
saurabh <saurabh.techie87@gmail.com>
2019-10-03 17:51:32 +0530

    implemented DI

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework;
 
+import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.MyController;
+import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,8 +14,8 @@ public class DiDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-		MyController controller = (MyController) ctx.getBean("myController");
-
-		controller.hello();
+		//System.out.println(((PropertyInjectedController) ctx.getBean("propertyInjectedController")).sayHello()) ;
+		//System.out.println(((SetterInjectedController) ctx.getBean("setterInjectedController")).sayHello()) ;
+		System.out.println(((ConstructorInjectedController) ctx.getBean("constructorInjectedController")).sayHello()) ;
 	}
 }
