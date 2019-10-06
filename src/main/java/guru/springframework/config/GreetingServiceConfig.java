a0f2353b68fa36d4c1ecf6c8c0018a7707fac648
saurabh <saurabh.techie87@gmail.com>
2019-10-07 01:07:19 +0530

    factory beans

---
+++ src/main/java/guru/springframework/config/GreetingServiceConfig.java
@@ -0,0 +1,39 @@
+package guru.springframework.config;
+
+import guru.springframework.services.GreetingRepository;
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository greetingRepository) {
+        return new GreetingServiceFactory(greetingRepository);
+    }
+
+    @Bean
+    @Profile({"default", "en"})
+    @Primary
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Profile("es")
+    @Primary
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Profile("de")
+    @Primary
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("de");
+    }
+}
