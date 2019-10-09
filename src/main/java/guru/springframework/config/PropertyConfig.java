d06d09e5acfe77b67bcc971acaf94fa35bb4a593
saurabh <saurabh.techie87@gmail.com>
2019-10-09 11:47:38 +0530

    externalized properties

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,35 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebean.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${sk.username}")
+    String user;
+    @Value("${sk.password}")
+    String password;
+    @Value("${sk.url}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
